1cc6a781ee62acf6a61f9a1c74d5c773035774cf
Davit Mkhitaryan <davitmkhitaryan1994@gmail.com/>
2022-05-13 16:09:20 +0200

    adding new test

--- src/homework/Test2.java
+++ src/homework/Test2.java
@@ -1,7 +1,7 @@
 package homework;
 
-public class Test {
+public class Test2 {
     public static void main(String[] args) {
-        System.out.println("Test changed");
+        System.out.println("Test2");
     }
 }
