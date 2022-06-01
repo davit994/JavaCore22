ea9b805d14f2592036b759a35caf24c545944ebd
Davit Mkhitaryan <davitmkhitaryan1994@gmail.com/>
2022-06-01 16:05:16 +0200

    chapter 5

---
+++ src/chapters/chapter5/ForEach.java
@@ -0,0 +1,13 @@
+package chapters.chapter5;
+
+public class ForEach {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int sum = 0;
+        for (int x : numbers) {
+            System.out.println("meaning = " + x);
+            sum += x;
+        }
+        System.out.println("sum = " + sum);
+    }
+}
