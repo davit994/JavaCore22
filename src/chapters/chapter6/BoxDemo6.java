8002860d04d6ef2162f6aaf34c1bf405bd4cf58e
Davit Mkhitaryan <davitmkhitaryan1994@gmail.com/>
2022-06-07 12:00:14 +0200

    add chapter 6, and calculator

---
+++ src/chapters/chapter6/BoxDemo6.java
@@ -0,0 +1,14 @@
+package chapters.chapter6;
+
+//public class BoxDemo6 {
+//    public static void main(String[] args) {
+//        Box mybox1 = new Box();
+//        Box mybox2 = new Box();
+//        double vol;
+//
+//        vol = mybox1.volume();
+//        System.out.println("The volume is " + vol);
+//        vol = mybox2.volume();
+//        System.out.println("The volume is " + vol);
+//    }
+//}
