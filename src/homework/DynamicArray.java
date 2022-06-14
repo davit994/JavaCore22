c27dfca10d96304fe9537dacb3675fe121e366f7
Davit Mkhitaryan <davitmkhitaryan1994@gmail.com/>
2022-06-15 00:35:51 +0200

    add homework

---
+++ src/homework/DynamicArray.java
@@ -0,0 +1,70 @@
+package homework;
+
+public class DynamicArray {
+
+    private int[] array = new int[10];
+    private int size = 0;
+
+    public void add(int value) {
+        if (array.length == size) {
+            extend();
+        }
+        array[size++] = value;
+    }
+
+    //Գրել մեթոդ isEmpty անունով, որը կվերադարձնի true եթե մեր dynamicArray-ի մեջ չունենանք ոչ մի էլեմենտ. Եթե ունենք՝ false
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    //Գրել մեթոդ getByIndex անունով, որը կընդունի ինդեքս, և կվերադարձնի այդ ինդեքսի տակ ընկած թիվը, եթե չկա թող վերադարձնի 0;
+    public int getByIndex(int index) {
+        return size - 1 >= index ? array[index] : 0;
+    }
+
+    //Գրել մեթոդ getFirstIndexByValue, որը կընդունի int value, և եթե մեր մասիվի մեջ ունենք այդ թվից, կվերադարձնի իր ինդեքսը. եթե շատ ունենք, պետք է վերադարձնել առաջինի ինդեքսը
+    public int getFirstIndexByValue(int value) {
+        for (int i = 0; i < size; i++) {
+            if (array[i] == value) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    //Գրել մեթոդ set անունով, որը կընդունի int index, int value և վալյուն կդնի տրված ինդեքսի տեղը(կփոխարինի էղած արժեքին).
+    public void set(int index, int value) {
+        array[index] = value;
+    }
+
+    //Գրել մեթոդ add(int index, int value) որը տրված վելյուն կդնի տրված ինդեքսի տեղը, իսկ էղած թիվը ու կողքի բոլոր թվերը կտանի աջ, ոչ մի թիվ չի կորի
+    public void add(int index, int value) {
+        for (int i = size; i >= index; i--) {
+            array[i] = array[i - 1];
+        }
+        array[index] = value;
+        size++;
+    }
+
+    //Գրել մեթոդ delete(int index) որ տանք ինդեքսը, այդ ինդեքսի տակ գտնվող թիվը հեռացնի մասիվից. (նոր մասիվ պետք չէ սարքել)
+    public void delete(int index) {
+        for (int i = index + 1; i < size; i++) {
+            array[i - 1] = array[i];
+        }
+        size--;
+    }
+
+    public void printArray() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+
+    private void extend() {
+        int[] temp = new int[array.length + 10];
+        for (int i = 0; i < array.length; i++) {
+            temp[i] = array[i];
+        }
+        array = temp;
+    }
+}
