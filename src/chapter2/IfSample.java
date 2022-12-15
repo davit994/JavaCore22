0153667bee8fcbf27b8d6156aef933e0e5e268e0
Davit Mkhitaryan <davitmkhitaryan1994@gmail.com/>
2022-12-15 21:12:38 +0100

    add chapters

---
+++ src/chapter2/IfSample.java
@@ -0,0 +1,19 @@
+package chapter2;
+
+public class IfSample {
+    public static void main(String[] args) {
+        int x, y;
+        x = 10;
+        y = 20;
+
+        if (x < y) System.out.println("x < y");
+
+        x = x * 2;
+        if (x == y) System.out.println("x = y");
+
+        x = x * 2;
+        if (x > y) System.out.println("x > y");
+
+        if (x == y) System.out.println("Этот текст не увидем");
+    }
+}
---
+++ src/chapter3/ScopeErr.java
@@ -0,0 +1,9 @@
+package chapter3;
+
+public class ScopeErr {
+    public static void main(String[] args) {
+
+        int bar = 1;
+        // int bar = 2; "Ошибка int bar уже определено"
+    }
+}
