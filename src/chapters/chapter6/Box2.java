7a9e71db4f84995234434035c765051ac12913b2
Davit Mkhitaryan <davitmkhitaryan1994@gmail.com/>
2023-01-14 15:15:54 +0100

    add chapter 6

---
+++ src/chapters/chapter6/Box2.java
@@ -0,0 +1,13 @@
+package chapters.chapter6;
+
+public class Box2 {
+
+    double width;
+    double height;
+    double depth;
+
+    void volume() {
+        System.out.print("Oбъeм равен");
+        System.out.println(width * height * depth);
+    }
+}
