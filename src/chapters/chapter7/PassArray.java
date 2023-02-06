311786956a05047fa6261fdec257dc008ba7d5f4
Davit Mkhitaryan <davitmkhitaryan1994@gmail.com/>
2023-02-06 21:42:27 +0100

    add chapter 7

---
+++ src/chapters/chapter7/PassArray.java
@@ -0,0 +1,16 @@
+package chapters.chapter7;
+
+public class PassArray {
+    static void vaтest(int v[]) {
+        System.out.print("Koличecтвo аргументов: " + v.length + " Содержимое: ");
+        for (int x : v)
+            System.out.print(x + " ");
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        int n1[] = {10};
+        int n2[] = {1, 2, 3};
+        int nЗ[] = {};
+    }
+}
